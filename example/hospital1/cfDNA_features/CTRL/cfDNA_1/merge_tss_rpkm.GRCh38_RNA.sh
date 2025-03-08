3124e686d0ed3a029c9a54d272a1d3af6554a2f6
wenqiuxu <xuwenqiu@genomics.cn>
2025-03-08 18:27:03 +0800

    Create merge_tss_rpkm.GRCh38_RNA.sh

---
+++ example/hospital1/cfDNA_features/CTRL/cfDNA_1/merge_tss_rpkm.GRCh38_RNA.sh
@@ -0,0 +1,4 @@
+for chr_name in {1..22}
+do
+cat split_chr/chr$chr_name/cfDNA_1.chr$chr_name.GRCh38_RNA.tss_rpkm >> /home/cfDNA_1/hospital1/cfDNA_features/CTRL/cfDNA_1/output/cfDNA_1.GRCh38_RNA.tss_rpkm
+done
