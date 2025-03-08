5f812d5f626f3532a9ce0fa3c41f8a45a990ddeb
wenqiuxu <xuwenqiu@genomics.cn>
2025-03-08 17:52:39 +0800

    Update pre_processing.sh

--- example/hospital1/cleandata/cfDNA_1/pre_processing.sh
+++ example/hospital1/cleandata/cfDNA_1/pre_processing.sh
@@ -1,2 +1,2 @@
 #####1. pre-processing#########################################
-SOAPnuke filter -f AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -r CAACTCCTTGGCTCACAGAACGACATGGCTACGATCCGACTT -1 /home/cfDNA/rawdata/CTRL/hospital1/cfDNA_1/cfDNA_1.read1.fq.gz -2 /home/cfDNA/rawdata/CTRL/hospital1/cfDNA_1/cfDNA_1.read2.fq.gz -o /home/cfDNA/cleandata/CTRL/hospital1/cfDNA_1 -C cfDNA_1.clean.read1.fq.gz -D cfDNA_1.clean.read2.fq.gz
+SOAPnuke filter -f AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -r CAACTCCTTGGCTCACAGAACGACATGGCTACGATCCGACTT -1 /home/cfDNA/hospital1/rawdata/CTRL/cfDNA_1/cfDNA_1.read1.fq.gz -2 /home/cfDNA/hospital1/rawdata/CTRL/cfDNA_1/cfDNA_1.read2.fq.gz -o /home/cfDNA/hospital1/cleandata/CTRL/cfDNA_1 -C cfDNA_1.clean.read1.fq.gz -D cfDNA_1.clean.read2.fq.gz
