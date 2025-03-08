823877c7ba82a627a8465692b51a513571176266
wenqiuxu <xuwenqiu@genomics.cn>
2025-03-08 18:12:07 +0800

    Delete example/hospital1 directory

--- example/hospital1/align_result/CTRL/cfDNA_1/align.sh
+++
@@ -1,3 +0,0 @@
-#####2.1 align pair end#########################################
-bwa mem -t 8 ~/reference/GRCh38/GRCh38_bwa/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna /home/cfDNA/hospital1/cleandata/CTRL/cfDNA_1/cfDNA_1.clean.read1.fq.gz /home/cfDNA/hospital1/cleandata/CTRL/cfDNA_1/cfDNA_1.clean.read2.fq.gz > /home/cfDNA/hospital1/align_result/CTRL/cfDNA_1/cfDNA_1.sam
-samtools view -@ 8 -o /home/cfDNA/hospital1/align_result/CTRL/cfDNA_1/cfDNA_1.bam /home/cfDNA/hospital1/align_result/CTRL/cfDNA_1/cfDNA_1.sam
--- example/hospital1/cleandata/CTRL/cfDNA_1/pre_processing.sh
+++
@@ -1,2 +0,0 @@
-#####1. pre-processing#########################################
-SOAPnuke filter -f AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -r CAACTCCTTGGCTCACAGAACGACATGGCTACGATCCGACTT -1 /home/cfDNA/hospital1/rawdata/CTRL/cfDNA_1/cfDNA_1.read1.fq.gz -2 /home/cfDNA/hospital1/rawdata/CTRL/cfDNA_1/cfDNA_1.read2.fq.gz -o /home/cfDNA/hospital1/cleandata/CTRL/cfDNA_1 -C cfDNA_1.clean.read1.fq.gz -D cfDNA_1.clean.read2.fq.gz
